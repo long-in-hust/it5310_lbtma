388b201cec328575bf70f57ed3b47db7bbd32f15
longlong <84077194+kirazero17@users.noreply.github.com>
2025-10-31 14:32:26 +0700

    simple switch needs sudo to use the veth

--- stm/scripts/run_mininet.sh
+++ stm/scripts/run_mininet.sh
@@ -19,7 +19,7 @@ echo "Starting BMv2 switch with gRPC..."
 simple_switch_grpc --device-id 0 --log-console -i 0@veth0 -i 1@veth1 $JSON_IMMEDIATE &
 
 # Debug command:
-# simple_switch_grpc --device-id 0 --log-console -i 0@veth0 -i 1@veth1 config/p4_stm.json/p4_stm.json
+# sudo simple_switch_grpc --device-id 0 --log-console -i 0@veth0 -i 1@veth1 config/p4_stm.json/p4_stm.json
 
 sleep 2
 
