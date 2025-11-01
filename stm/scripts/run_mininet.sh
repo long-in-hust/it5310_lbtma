1892a992045ac37a4c4466face873e69432194ce
kirazero17 <84077194+kirazero17@users.noreply.github.com>
2025-11-01 08:58:26 +0000

    changed

--- stm/scripts/run_mininet.sh
+++ stm/scripts/run_mininet.sh
@@ -14,23 +14,16 @@ echo "Compiling P4 program..."
 p4c --target bmv2 --arch v1model --p4runtime-files $P4INFO --std p4-16 -o $JSON $P4SRC
 # p4c --target bmv2 --arch v1model --p4runtime-files config/p4_stm.p4info.txt --std p4-16 -o ./config p4_src/p4_stm.p4
 
-# Launch BMv2 switch
+# Launch BMv2 switch - May run this after the topology command
 echo "Starting BMv2 switch with gRPC..."
-simple_switch_grpc --device-id 0 --log-console -i 0@veth0 -i 1@veth1 $JSON_IMMEDIATE &
+simple_switch_grpc --device-id 0 --log-console -i 1@s1-eth1 -i 2@s1-eth2 $JSON_IMMEDIATE &
 
 # Debug command:
-# sudo simple_switch_grpc --device-id 0 --log-console -i 0@veth0 -i 1@veth1 config/p4_stm.json/p4_stm.json
+# sudo simple_switch_grpc --grpc_port 50051 --device-id 0 --log-console -i 1@s1-eth1 -i 2@s1-eth2 config/p4_stm.json/p4_stm.json
 
 sleep 2
 
-# Launch controller (Python gRPC)
-# echo "Launching P4Runtime controller..."
-# cd ../control
-# python3 controller.py &
-# cd -
-# sleep 2
-
-# Launch Mininet topology
+# Launch Mininet topology - May run this before the simple switch command
 echo "Starting Mininet..."
 sudo python3 ../topology/topology.py
 # sudo python3 topology.py
