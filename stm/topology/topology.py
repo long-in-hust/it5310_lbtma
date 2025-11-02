5d3ed89256fffe5e0328fe47c9d6e4c8401144c9
kirazero17 <84077194+kirazero17@users.noreply.github.com>
2025-11-02 19:03:25 +0700

    fixed a bit more

--- stm/topology/topology.py
+++ stm/topology/topology.py
@@ -6,24 +6,11 @@ from mininet.node import RemoteController
 from mininet.link import TCLink
 from mininet.cli import CLI
 from mininet.log import setLogLevel
-from p4utils.mininetlib.node import P4Switch
-
-class SimpleP4Switch(P4Switch):
-    def __init__(self, name, p4_src, **kwargs):
-        super(SimpleP4Switch, self).__init__(name, p4_src="../p4_src/p4_stm.p4", grpc_port=50051, **kwargs)
 
 class STMTopo(Topo):
     def build(self):
         h1 = self.addHost('h1', ip='10.0.1.1/24')
         h2 = self.addHost('h2', ip='10.0.2.2/24')
-
-        # s1 = self.addSwitch('s1', 
-        #                    cls=SimpleP4Switch, # Replace with your P4 switch class
-        #                    device_id=0,
-        #                    p4_src="../p4_src/p4_stm.p4", 
-        #                    json_path="../config/p4_stm.json",
-        #                    pipeconf="org.onosproject.pipelines.basic",
-        #                    thrift_port=9090)
 
         s1 = self.addSwitch('s1')
 
