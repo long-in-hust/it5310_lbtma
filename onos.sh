7a59c0f2f09d47643a02b66b912772dc92364d58
kirazero17 <84077194+kirazero17@users.noreply.github.com>
2025-11-06 21:39:02 +0700

    fixed a lot

--- onos.sh
+++ onos.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
 docker run -t -d -p 8181:8181 -p 8101:8101 -p 5005:5005 -p 830:830 -p 6653:6653 --name onos8 onosproject/onos
+
+curl --fail -sSL --user onos:rocks --noproxy p4.it530.hust.local \
+    -X POST -HContent-Type:application/octet-stream \
+	'http://p4.it530.hust.local:8181/onos/v1/applications?activate=true' \
+	--data-binary @p4-stm-0.0.1.oar
--- onos/netcfg/api.sh
+++ onos/netcfg/api.sh
@@ -1,1 +1,3 @@
-curl -X POST -H "content-type:application/json" http://localhost:8181/onos/v1/network/configuration -d @netcfg.json --user onos:rocks
+curl -X POST -H "content-type:application/json" \
+    http://p4.it530.hust.local:8181/onos/v1/network/configuration \
+    -d @netcfg.json --user onos:rocks
